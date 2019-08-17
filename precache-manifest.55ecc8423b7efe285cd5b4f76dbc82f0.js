b2b415c8ae817d3af533c47f2eb8f48c881eb0de
Olufeyimi Gbenga <gbqngq@gmail.com>
2019-08-17 08:34:34 +0100

    Updates

---
+++ precache-manifest.55ecc8423b7efe285cd5b4f76dbc82f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4c743789ab40bcfd4f758e0d7ba80c",
+    "url": "/image-recognition/index.html"
+  },
+  {
+    "revision": "571d6c7eba854d848210",
+    "url": "/image-recognition/static/css/main.86afe79d.chunk.css"
+  },
+  {
+    "revision": "7870c54a14a57562b584",
+    "url": "/image-recognition/static/js/2.8eadb9ed.chunk.js"
+  },
+  {
+    "revision": "571d6c7eba854d848210",
+    "url": "/image-recognition/static/js/main.362c29ee.chunk.js"
+  },
+  {
+    "revision": "da236e1df4f0408c52b8",
+    "url": "/image-recognition/static/js/runtime~main.1bab8aa3.js"
+  }
+]);
